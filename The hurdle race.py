eca9a9ed4d7590645a8b99f90d06fab80dfb165c
SakethGudi <68059292+SakethGudi@users.noreply.github.com>
2021-04-27 18:17:03 +0530

    Add files via upload

---
+++ The hurdle race.py
@@ -0,0 +1,33 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+def hurdleRace(k, height):
+    m=max(height)
+    doses=0
+    if(k<m):
+        doses=doses+m-k
+    else:
+        doses=doses
+    return doses
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    height = list(map(int, input().rstrip().split()))
+
+    result = hurdleRace(k, height)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
